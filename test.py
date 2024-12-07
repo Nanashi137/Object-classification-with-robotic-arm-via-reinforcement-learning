0e1e49fbef3b613339da84b87576bfda85276240
Nanashi137 <c0mputereng1neerstudent0@gmail.com>
2024-12-07 13:35:30 +0700

    PPO usage example

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from agent import Resnet10, PPO, Actor, Critic
 from ultilities import config_parser
 import torch
 from torch.utils.tensorboard import SummaryWriter
-
+from agent import Resnet10
 
 if __name__=="__main__":
 
@@ -16,15 +16,18 @@ if __name__=="__main__":
 
     actor  = Actor(arch=backbone, n_joints=n_joints)
     critic = Critic(arch=backbone)
+    dummy_state = torch.randn(1, 1,224, 224)
     
-    logging = SummaryWriter(log_dir="./temp")
+    logging = SummaryWriter(log_dir="./temp") # initialize tensorboard summary writer
 
     ppo = PPO(actor=actor, critic=critic, logging=logging)
 
-    dummy_state = torch.randn(5, 1,224, 224)
 
     means, stds = actor(dummy_state)
     print(means.shape)
     print(stds.shape)
+    
+    logging.close() # close the writer 
+
 
 
